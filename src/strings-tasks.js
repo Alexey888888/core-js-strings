03928e9f16445aa1581ca7cf11d1ade148a40c95
Alexey888888 <brdkmail@gmail.com>
2023-11-07 17:04:29 +0200

    feat: done the first 11 tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -185,8 +185,13 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (!str) return 0;
+  let sum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    sum += str[i].charCodeAt();
+  }
+  return sum;
 }
 
 /**
